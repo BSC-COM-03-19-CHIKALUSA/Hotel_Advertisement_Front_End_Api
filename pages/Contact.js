8b02288850846185afeb24ba2807b0086367c7fa
bsc-com-03-19 <maxwellchikalusa@gmail.com>
2022-02-27 01:52:13 +0200

    pushed HOME,ABOUT,CONTACT ,SERVICES AND NOTIFICATION

---
+++ pages/Contact.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Contact() {
+    
+    return <h1 className='contacts'>Contact
+    
+    
+                        <h2>YOU CAN USE THE FOLLOWING TO CONTACT US</h2>
+                       <h3> ****TELEPHONE_NO*****0198765887****</h3>,
+                        <h3>*******SEND_EMAIL*****greathotel@gmail.com******</h3>,
+                       <h3> *******WRITE A LETTER*****Great hotel,box 200,lilongwe****</h3>,
+                        <h3> *******USE_WHATSAPP******2659987678987*****</h3>
+                    
+                        </h1>;
+    
+    
+}
---
+++ pages/Notification.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Notification() {
+    return <h1 className='notification'>Notification 
+    
+    
+    
+                        <h2>PLEASE READ THE FOLLOWING NOTIFICATION TO HAVE MORE DATA</h2>
+                       <h3> *******EXPENSIVE_ROOMS_ARE @ MWK15,000 EACH*********</h3>,
+                        <h3>*******MEDIUM_CONFORTABLE_ROOMS ARE @ MWK 10,000 EACH************</h3>,
+                       <h3> *******CHEAP_ROOMS_ARE @ MWK7,000 EACH*********</h3>,
+                        <h3> ********BOOK YOUR'S AND ENJOY THE HOTEL**********</h3>
+                    
+                        </h1>;
+    
+    
+    
+}
